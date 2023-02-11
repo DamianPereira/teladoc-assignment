65c33117dc7bc0e1c1307a0eccde280e3e5da65b
Damian Marcelo Pereira <damian.pereira@10pines.com>
2023-02-10 22:21:04 -0300

    Implement 1 possible solution for problem 2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 describe User, type: :model do
   let(:some_first_name) {'John'}
   let(:some_last_name) {'Smith'}
+  let(:some_full_name) {'John Smith'}
   let(:some_user) { User.create(first_name: some_first_name, last_name: some_last_name) }
   it "has a first name" do
     expect(some_user.first_name).to eq(some_first_name)
   end
   it "has a last name" do
     expect(some_user.last_name).to eq(some_last_name)
+  end
+  it "has a full name" do
+    expect(some_user.full_name).to eq(some_full_name)
   end
 end
