50292795d9598888ba04be35c047beacd9d34699
Damian Marcelo Pereira <damian.pereira@10pines.com>
2023-02-10 22:15:12 -0300

    Complete first problem

--- app/lib/utils.rb
+++ app/lib/utils.rb
@@ -1,5 +1,11 @@
 class Utils
-  def self.filter_and_group
-
+  def self.filter_and_group(string_list, integer)
+    return {} if integer < 2
+    filtered_strings = string_list.filter { |string| string.length < integer }
+    count_values = (1..integer - 1).map { |value| { value => 0 } }
+    initial_group_hash = count_values.reduce(:merge)
+    filtered_strings.reduce(initial_group_hash) do |result, string|
+      result.tap { result[string.length] += 1 }
+    end
   end
 end
--- spec/filter_and_group_spec.rb
+++ spec/filter_and_group_spec.rb
@@ -1,7 +1,35 @@
 require "rails_helper"
 
 describe Utils do
-  it 'runs' do
-    Utils.filter_and_group
+  let(:string_list) { %w[a b c yy abc abc abcdef ghjklw] }
+  let(:integer) { 4 }
+  subject { Utils.filter_and_group(string_list, integer) }
+
+  context 'when the integer is 0' do
+    let(:integer) { 0 }
+    it "returns an empty Hash" do
+      is_expected.to eq({})
+    end
+  end
+
+  context 'when the integer is 1' do
+    let(:integer) { 1 }
+    it "returns an empty Hash" do
+      is_expected.to eq({})
+    end
+  end
+
+  context "when the string list has no elements" do
+    let(:string_list) { [] }
+    it "returns an Hash with each count set to 0 up to integer - 1" do
+      is_expected.to eq({ 1 => 0, 2 => 0, 3 => 0 })
+    end
+  end
+
+  context "when string list has values and integer is more than 2" do
+    it "returns a count of each word that has less than integer length" do
+      is_expected.to eq({ 1 => 3, 2 => 1, 3 => 2 })
+    end
   end
 end
+
